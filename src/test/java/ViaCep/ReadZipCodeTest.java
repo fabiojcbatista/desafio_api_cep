25e236e4a9dcfd39c345a3801287d97b9065be32
Fabio Jr <fabiojcbweb@gmail.com>
2022-12-09 20:03:42 -0300

    Add configuration allure report.

--- src/test/java/ViaCep/ReadZipCodeTest.java
+++ src/test/java/ViaCep/ReadZipCodeTest.java
@@ -14,54 +14,54 @@ import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
 
 public class ReadZipCodeTest extends TestBases {
-    private static final String validZipCode = FileOperation.getProperties("Variables").getProperty("validZipCode");
-    private static final String invalidZipCode = FileOperation.getProperties("Variables").getProperty("invalidZipCode");
+    private static final String VALID_ZIPCODE = FileOperation.getProperties("Variables").getProperty("validZipCode");
+    private static final String INVALID_ZIPCODE = FileOperation.getProperties("Variables").getProperty("invalidZipCode");
+    public static final String VALID_URI = BASE_URI+PATH_WS+"/" + VALID_ZIPCODE + PATH_FORMAT;
+    private  static final String INVALID_URI = BASE_URI+PATH_WS+"/" + INVALID_ZIPCODE + PATH_FORMAT;
+
 
     @Description("Test valid Zip Code")
     @DisplayName("Creating test for valid Zip Code")
     @Step("Valid ZipCode: {validZipCode}.")
-    @Link("{BASE_URI}/{PATH_WS}/{validZipCode}/{PATH_FORMAT}")
+    @Link("https://viacep.com.br/ws/94440360/json")
     @Story("Test get Zip Code")
     @Test
     public void getValidZipCodeTest(){
-        Response response = given()
-                .spec(requestSpec)
-                .basePath(PATH_WS)
-                .log().body()
+        Response response =
+                given()
+                    .spec(requestSpec)
+                    .basePath(PATH_WS)
+                    .log().body()
                 .when()
-                .get("/" + validZipCode + PATH_FORMAT)
+                    .get("/" + VALID_ZIPCODE + PATH_FORMAT)
                 .then()
-                .spec(responseSpec)
-                .and()
-                .log()
-                .body()
-                .statusCode(200)
-                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Schemas/json-schema.json"))
-                .extract().response();
-
+                    .spec(responseSpec)
+                    .and()
+                    .log()
+                    .body()
+                    .statusCode(200)
+                    .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Schemas/json-schema.json"))
+                    .extract().response();
         System.out.println(response.then().extract().path("cep").toString());
-
-
     }
-
 
     @Description("Test invalid Zip Code")
     @DisplayName("Creating test for invalid Zip Code")
     @Step("Invalid ZipCode: {invalidZipCode}.")
-    @Link("{BASE_URI}/{PATH_WS}/{invalidZipCode}/{PATH_FORMAT}")
+    @Link("https://viacep.com.br/ws/00000000/json")
     @Story("Test get Zip Code")
     @Test
     public void getInvalidZipCodeTest(){
          given()
-                .spec(requestSpec)
-                .basePath(PATH_WS)
-                 .when()
-                .get("/" + invalidZipCode + PATH_FORMAT)
-                .then()
-                .spec(responseSpec)
-                .log()
-                 .status()
-                 .statusCode(200)
-                 .body("erro" , equalTo (true));
+            .spec(requestSpec)
+            .basePath(PATH_WS)
+         .when()
+            .get("/" + INVALID_ZIPCODE + PATH_FORMAT)
+         .then()
+            .spec(responseSpec)
+            .log()
+            .status()
+            .statusCode(200)
+            .body("erro" , equalTo (true));
     }
 }
